8b20f6dcc06e7e6a607abbeaa822f923b9e372d6
Frank <mail@frank-mayer.io>
2022-03-20 11:33:56 +0100

    konverted to kotlin

---
+++ src/main/java/io/frankmayer/naruto/Naruto.kt
@@ -0,0 +1,13 @@
+package io.frankmayer.naruto
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Naruto : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
